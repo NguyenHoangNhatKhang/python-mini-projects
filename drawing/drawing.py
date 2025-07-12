7177e94fcbb5c1193ed162402d7e7b1025facb3c
NguyenHoangNhatKhang <nghoangnhkhang@gmail.com>
2025-07-12 18:41:58 +0900

    upload drawing

---
+++ drawing/drawing.py
@@ -0,0 +1,40 @@
+from turtle import Turtle , Screen
+artist_is_drawing = Turtle()
+for _ in range(3):
+    artist_is_drawing.forward(100)
+    artist_is_drawing.right(120)
+
+for _ in range(4):
+    artist_is_drawing.forward(100)
+    artist_is_drawing.right(90)
+
+for _ in range(5):
+    artist_is_drawing.forward(100)
+    artist_is_drawing.right(72)
+
+for _ in range(6):
+    artist_is_drawing.forward(100)
+    artist_is_drawing.right(60)
+
+for _ in range(7):
+    artist_is_drawing.forward(100)
+    artist_is_drawing.right(51.4285714)
+
+for _ in range(8):
+    artist_is_drawing.forward(100)
+    artist_is_drawing.right(45)
+
+for _ in range(9):
+    artist_is_drawing.forward(100)
+    artist_is_drawing.right(40)
+
+
+for _ in range(10):
+    artist_is_drawing.forward(100)
+    artist_is_drawing.right(36)
+
+                            
+
+
+screen = Screen()
+screen.exitonclick()
