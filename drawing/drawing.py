c92078c6cbfc56e4c67303a922c266a852ac9c2e
NguyenHoangNhatKhang <nghoangnhkhang@gmail.com>
2025-07-12 19:35:17 +0900

    Update drawing.py

--- drawing/drawing.py
+++ drawing/drawing.py
@@ -1,40 +1,20 @@
 from turtle import Turtle , Screen
-artist_is_drawing = Turtle()
-for _ in range(3):
-    artist_is_drawing.forward(100)
-    artist_is_drawing.right(120)
+from random import choice
+tim = Turtle()
 
-for _ in range(4):
-    artist_is_drawing.forward(100)
-    artist_is_drawing.right(90)
-
-for _ in range(5):
-    artist_is_drawing.forward(100)
-    artist_is_drawing.right(72)
-
-for _ in range(6):
-    artist_is_drawing.forward(100)
-    artist_is_drawing.right(60)
-
-for _ in range(7):
-    artist_is_drawing.forward(100)
-    artist_is_drawing.right(51.4285714)
-
-for _ in range(8):
-    artist_is_drawing.forward(100)
-    artist_is_drawing.right(45)
-
-for _ in range(9):
-    artist_is_drawing.forward(100)
-    artist_is_drawing.right(40)
+colors = ["medium turquoise","khaki","azure","medium blue","lime green","aquamarine"]
 
 
-for _ in range(10):
-    artist_is_drawing.forward(100)
-    artist_is_drawing.right(36)
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(70)
+        tim.right(angle)
 
-                            
 
-
+for shape_side_n in range(3,11):
+    tim.color(choice(colors))
+    draw_shape(shape_side_n)
+    
 screen = Screen()
 screen.exitonclick()
