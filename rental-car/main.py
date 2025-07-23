9c8df0d616407f994c90b312d238bd6fcd8dba4b
NguyenHoangNhatKhang <nghoangnhkhang@gmail.com>
2025-07-24 02:45:41 +0900

    Update main.py

--- rental-car/main.py
+++ rental-car/main.py
@@ -2,23 +2,15 @@ from veh_class import Vehicle,Car,MotorBike,Bike
 from veh_data import vehicles
 need_vehicle = True
 while need_vehicle:
+    found = False
     vehicle_type = input("Whats the type of the vehicle you want to rent?: ").strip().lower()
-    if vehicle_type == "car":
-        for vehicle in vehicles:
-            if vehicle_type == vehicle.type.lower():   
-                vehicle.display()
+    for vehicle in vehicles:
+        if vehicle_type == vehicle.type.lower():
+            vehicle.display()
+            found = True 
     
-    elif vehicle_type == "bike":
-        for vehicle in vehicles:
-            if vehicle_type == vehicle.type.lower():
-                vehicle.display()
-    
-
-    elif vehicle_type == "motorbike":
-        for vehicle in vehicles:
-            if vehicle_type == vehicle.type.lower():
-                vehicle.display()
-
-    
-    else:
-        print(f"{vehicle_type} is not found!!!")
+    if not found: 
+        print("== Available types: car / motorbike / bike ==")
+        print(f"{vehicle_type} is not found!")
+        
+            
