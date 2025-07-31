b151478241965b527c63cf5ea80fcb9662c0724f
NguyenHoangNhatKhang <nghoangnhkhang@gmail.com>
2025-07-31 21:23:04 +0900

    update

--- snack-game/score.py
+++ snack-game/score.py
@@ -7,6 +7,7 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.goto(0,270)
         self.color("white")
@@ -14,12 +15,18 @@ class Score(Turtle):
         self.hideturtle()
     
     def update_score(self):
-        self.write(f"Score: {self.score}",align=ALLIGNMENT,font=(FONT,SIZE,STYLE))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}",align=ALLIGNMENT,font=(FONT,SIZE,STYLE))
     def increase_score(self):
         self.score +=  1 
         self.clear()
         self.update_score()
     
+    def reset(self):
+        if self.score >= self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
     def game_over(self): 
         self.clear()
         self.goto(0,0)
